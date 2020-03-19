10c0b33421ae215893d9373b7443bc3a4e2e53e0
Dave R. Tolentin <dave@clearex.com.ph>
2020-03-19 16:13:35 +0800

    Rearrange the menu and add new modules View Companies, Buy Stocks, Deposit, View Deposits and Cash Balance.

--- COL/Program.cs
+++ COL/Program.cs
@@ -27,6 +27,7 @@ namespace COL
         private static readonly string REC_EXIST = "code already exist!";
         private static readonly string REC_NOT_EXIST = "code doest not exist!";
         private static readonly string INVALID_OPTION = "Invalid option!";
+        private static readonly string DATE_FORMAT = "MM/dd/yyyy";
         static Menu()
         {
 
@@ -38,10 +39,22 @@ namespace COL
             return new StringBuilder(max).Insert(0, " ", max - value.Length).ToString();
         }
 
+        private static string parseDate(string date, string format)
+        {
+            return DateTime.Parse(date).ToString(format);
+        }
+
         public static void display(MySqlConnection conn)
         {
             int option = 0;
-            Console.WriteLine("[1] New Company\r\n[2] Stocks \r\n[3] Portfolio\r\n[0] Exit");
+            Console.WriteLine("[1] New Company\r\n" +
+                "[2] View Companies\r\n" +
+                "[3] Buy Stocks\r\n" +
+                "[4] Portfolio\r\n" +
+                "[5] Deposit\r\n" +
+                "[6] View Deposits\r\n" +
+                "[7] Cash Balance\r\n" +
+                "[0] Exit");
             do
             {
                 try
@@ -81,7 +94,25 @@ namespace COL
                 }
                 else if (option == 2)
                 {
-                    Console.WriteLine("* Stocks *");
+                    Console.WriteLine("* View Companies *");
+                    MySqlDataReader rdr = Companies.getAllCompanies(conn);
+                    // TO DO: Must be dynamic
+                    Console.WriteLine("+-----------------+--------------------------------------------+\r\n" +
+                        "+ Company Code    | Company Description                        +\r\n"+
+                        "+-----------------+--------------------------------------------+");
+                    while (rdr.Read() && rdr != null)
+                    {
+                        string compCode = rdr["comp_code"].ToString();
+                        string compName = rdr["comp_name"].ToString();
+                        Console.WriteLine("| "+ compCode + strRepeat(compCode, 15)+" | "+compName+ strRepeat(compName, 43) +"|");
+                    }
+                    Console.WriteLine("+-----------------+--------------------------------------------+");
+
+                    rdr.Close();
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("* Purchase Stocks *");
                     string compCode;
                     double price;
                     int qty;
@@ -123,7 +154,7 @@ namespace COL
                     }
                     Menu.display(conn);
                 }
-                else if (option == 3)
+                else if (option == 4)
                 {
                     string compCode = "";
                     Console.WriteLine("* Portfolio *");
@@ -144,12 +175,13 @@ namespace COL
                         }
                         compRdr.Close();
                         Console.WriteLine(compName);
+                        Console.WriteLine("Note: Comm + VAT and Other Charges are not included in the calculation!");
                         MySqlDataReader rdr = Stocks.getStocksByCompCode(conn, compCode);
-                        // TO DO: Must dynamic
+                        // TO DO: Must be dynamic
                         Console.WriteLine("+--------------+-----------+---------+-------------------+-------------------------+-------------+---------------+----------------+\r\n" +
                             "+     Date     |   Price   |   Qty   |  Amount Invested  |  Total Amount Invested  |  Total Qty  |  Stock Value  |  Gain or Loss  +\r\n" +
                             "+--------------+-----------+---------+-------------------+-------------------------+-------------+---------------+----------------+");
-                        DataTable dtStocks = new DataTable(); // Optional to use the Datatable here, we can use directly the rdr.Read()
+                        DataTable dtStocks = new DataTable(); // Optional to use the DataTable here, we can use directly the rdr.Read()
                         dtStocks.Load(rdr);
 
                         double totalAmtInvested = 0.00;
@@ -157,7 +189,6 @@ namespace COL
                         String data = "";
                         for (int i = 0; i < dtStocks.Rows.Count; i++)
                         {
-                            DateTime dt = DateTime.Parse(dtStocks.Rows[i]["date"].ToString());
                             int qty = Convert.ToInt32(dtStocks.Rows[i]["qty"]);
                             double price = Convert.ToDouble(dtStocks.Rows[i]["price"]);
                             double amtInvested = qty * price;
@@ -167,7 +198,7 @@ namespace COL
                             double stockValue = totalQty * price;
                             double gainOrLoss = stockValue - totalAmtInvested;
 
-                            data += "| " + dt.ToString("MM/dd/yyyy") + "   | " +
+                            data += "| " + parseDate(dtStocks.Rows[i]["date"].ToString(), DATE_FORMAT) + "   | " +
                                 price + strRepeat(price.ToString(), 10) + "| " +
                                 qty + strRepeat(qty.ToString(), 8) + "| " +
                                 Math.Round(amtInvested, 2) + strRepeat(amtInvested.ToString(), 18) + "| " +
@@ -182,6 +213,61 @@ namespace COL
                         rdr.Close();
                     }
                 }
+                else if (option == 5)
+                {
+                    Console.WriteLine("* Deposit *");
+                    double amt;
+                    Console.WriteLine("Amount: ");
+                    try
+                    {
+                        amt = Convert.ToDouble(Console.ReadLine());
+                    } catch (Exception ex)
+                    {
+                        amt = 0.00;
+                    }
+                    if (Deposits.insert(conn, amt))
+                    {
+                        Console.WriteLine(SUCCESS_SAVE);
+                    }
+                    else
+                    {
+                        Console.WriteLine(ERROR_SAVE);
+                    }
+                    Menu.display(conn);
+                }
+                else if (option == 6)
+                {
+                    Console.WriteLine("* View Deposits *");
+                    MySqlDataReader rdr = Deposits.getAllDeposits(conn);
+                    // TO DO: Must be dynamic
+                    Console.WriteLine("+--------------+-----------+\r\n" +
+                        "+     Date     | Amount    +\r\n"+
+                        "+--------------+-----------+");
+                    double totalDeposit = 0.00;
+                    while (rdr.Read() && rdr != null)
+                    {
+                        double amount = Convert.ToDouble(rdr["amount"]);
+                        totalDeposit += amount;
+                        Console.WriteLine("| "+ parseDate(rdr["date"].ToString(), DATE_FORMAT)+"   | "+ amount + strRepeat(amount.ToString(), 10)+"|");
+                    }
+                    Console.WriteLine("+--------------+-----------+");
+                    Console.WriteLine("Total Deposit: "+totalDeposit);
+
+                    rdr.Close();
+                }
+                else if (option == 7)
+                {
+                    Console.WriteLine("* Cash Balance *");
+                    Console.WriteLine("Note: Cash Balance is not equal to COL Account because COMM + VAT & Other Charges is not deducted when buying stocks!");
+                    MySqlDataReader rdr = Deposits.getCashBalance(conn);
+                    while (rdr.Read() && rdr != null)
+                    {
+                        Console.WriteLine("Total Deposit: "+rdr["total_deposits"]);
+                        Console.WriteLine("Total Invested: "+rdr["total_invested"]);
+                        Console.WriteLine("Cash Balance: "+rdr["cash_balance"]);
+                    }
+                    rdr.Close();
+                }
                 else if (option == 0)
                 {
                     Environment.Exit(0);
@@ -190,7 +276,7 @@ namespace COL
                 {
                     Console.WriteLine(INVALID_OPTION);
                 }
-            } while (option != 1 || option != 2 || option != 3 || option != 0);
+            } while (option != 1 || option != 2 || option != 3 || option != 4 || option != 5 || option != 6 || option != 7 || option != 0);
         }
     }
 
@@ -249,6 +335,14 @@ namespace COL
             return rdr;
         }
 
+        public static MySqlDataReader getAllCompanies(MySqlConnection conn)
+        {
+            string sql = "SELECT * FROM companies";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            return cmd.ExecuteReader();
+        }
+
         public static bool isCompCodeExist(MySqlConnection conn, string compCode)
         {
             string sql = "SELECT * FROM companies WHERE comp_code = @CompCode";
@@ -274,7 +368,7 @@ namespace COL
 
         }
 
-        public static bool insert(MySqlConnection conn, String compCode, double price, int qty)
+        public static bool insert(MySqlConnection conn, string compCode, double price, int qty)
         {
             string sql = "INSERT INTO stocks (comp_code, price, qty, date) VALUES (@CompCode, @Price, @Qty, @Date)";
             MySqlCommand cmd = new MySqlCommand(sql, conn);
@@ -298,6 +392,55 @@ namespace COL
             MySqlDataReader rdr = cmd.ExecuteReader();
             
             return rdr;
+        }
+
+        public static MySqlDataReader getAllStocks(MySqlConnection conn)
+        {
+            String sql = "SELECT * FROM stocks";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            return cmd.ExecuteReader();
+        }
+    }
+
+    class Deposits
+    {
+        static Deposits()
+        {
+
+        }
+
+        public static bool insert(MySqlConnection conn, double amt)
+        {
+            String sql = "INSERT INTO deposits (amount, date) VALUES (@Amount, @Date)";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Amount", amt);
+            cmd.Parameters.AddWithValue("@Date", DateTime.Now);
+
+            if (cmd.ExecuteNonQuery() < 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static MySqlDataReader getCashBalance(MySqlConnection conn)
+        {
+            String sql = "SELECT *, (total_deposits - total_invested) AS cash_balance FROM (SELECT SUM(total) AS total_invested, " +
+                "(SELECT SUM(amount) FROM deposits) AS total_deposits" +
+                " FROM(SELECT *, (price * qty) AS total FROM stocks) AS m) main";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            return cmd.ExecuteReader();
+        }
+
+        public static MySqlDataReader getAllDeposits(MySqlConnection conn)
+        {
+            String sql = "SELECT * FROM deposits";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            return cmd.ExecuteReader();
         }
     }
 }
